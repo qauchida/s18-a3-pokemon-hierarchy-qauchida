4bd04c61c2e148c959da6e27860b5ac443060ac3
Quinne Uchida <qauchida@hawaii.edu>
2018-02-07 03:00:16 -1000

    Charmander

---
+++ Charmander.java
@@ -0,0 +1,11 @@
+/**
+*@qauchida
+*@since 02/07/18
+*makes charmander
+*/
+
+public class Charmander extends Pokemon implements FireType{
+
+   public Charmander(){   
+   } //close 
+} //close class
