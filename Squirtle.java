cd268a575ee292ff89c00c38d39558bd1b4dabfb
Quinne Uchida <qauchida@hawaii.edu>
2018-02-07 03:31:18 -1000

    Pokemon Files
    remade squirtle

--- Squirtle.java
+++ Squirtle.java
@@ -2,10 +2,10 @@ import java.util.*;
 /**
 *@qauchida
 *@since 02/07/18
-*squirtle
+*Squirtle
 */
+
 public class Squirtle extends Pokemon implements WaterType{
-   public Squirtle(){
-   
-   }
+
+
 } //close class
