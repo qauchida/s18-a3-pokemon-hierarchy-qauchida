179b94fd9e4d7407085ac2b7dedbc8d0676bbb26
Quinne Uchida <qauchida@hawaii.edu>
2018-02-13 20:51:56 -1000

    Pokemon Files
    Squirtle: 21/21
    Wartortle: 21/21
    Blastoise: 21/21
    Charmander: 21/21
    Charmeleon: 20/21

--- Charmeleon.java
+++ Charmeleon.java
@@ -25,17 +25,8 @@ public class Charmeleon extends Charmander{
          super("Charmeleon", name, 5, 1.1, 19.0, BASE_ATTACK_POWER, 
              BASE_DEFENSE_POWER, BASE_STAMINA_POWER);
    }
-   /** 
-   * Constructor with species and name for subclasses.
-   *@param species The Pokemon's species.
-   *@param name The optional user-given name.
-   *@param num The Pokedex number for this species.
-   *@param ht The height of this Pokemon.
-   *@param wt The weight of this Pokemon. 
-   *@param baseAttackPwr The low limit of Attack Power for species.
-   *@param baseDefensePwr The low limit of Defense Power for species.
-   *@param baseStaminaPwr The low limit of Stamina Power for speices. 
-   */
+
+
    protected Charmeleon(String species, String name, int num, double ht, 
        double wt, int baseAttackPwr, int baseDefensePwr, int baseStaminaPwr) {
       
