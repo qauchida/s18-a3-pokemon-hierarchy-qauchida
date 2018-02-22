b0ead854128c6abb2b63eacde391ba65c5d4bf75
Quinne Uchida <qauchida@hawaii.edu>
2018-02-22 13:31:50 -1000

    fixing squirtle checkstyle

--- Squirtle.java
+++ Squirtle.java
@@ -5,47 +5,70 @@ import java.util.*;
 *Squirtle
 */
 
-public class Squirtle extends Pokemon implements WaterType,NullType{
+public class Squirtle extends Pokemon implements WaterType, NullType {
 
-/**
-*declare base stats as constants
-*/
+   /**
+   *declare base stats as constants.
+   */
    static final int ATTACK = 94;
+   /***/
    static final int DEFENSE = 122;
+   /***/
    static final int STAMINA = 88; 
-   
+   /***/
    protected boolean fastIsWater = true;
+   /***/
    protected boolean specialIsWater = true;
    
-   /**no name constructor 
+   /**
+   *no name constructor. 
    */
-   public Squirtle(){
-      super("Squirtle","Squirtle", 7, WATER_COLOR, 0.5, 9.0, 
-         WATER_TYPE,NULL_TYPE, ATTACK, DEFENSE, STAMINA);
-      chooseFastAttack();
-      chooseSpecialAttack();
-   } // close constructor
-   
-      /**with name constructor 
-   */
-   public Squirtle(String name){
-      super("Squirtle",name, 7, WATER_COLOR, 0.5, 9.0, 
+   public Squirtle() {
+      super("Squirtle", "Squirtle", 7, WATER_COLOR, 0.5, 9.0, 
          WATER_TYPE, NULL_TYPE, ATTACK, DEFENSE, STAMINA);
       chooseFastAttack();
       chooseSpecialAttack();
    } // close constructor
    
+   /**
+   *with name constructor. 
+   *@param name 
+   *@param num
+   *@param ht
+   *@param wt
+   *@param defense
+   *@param stamina
+   */
+   public Squirtle(String name) {
+      super("Squirtle", name, 7, WATER_COLOR, 0.5, 9.0, 
+         WATER_TYPE, NULL_TYPE, ATTACK, DEFENSE, STAMINA);
+      chooseFastAttack();
+      chooseSpecialAttack();
+   } // close constructor
+   
+   /**
+   *with name constructor. 
+   *@param species Squirtle
+   *@param num 7
+   *@param ht 0.5
+   *@param wt 9.0
+   *@param baseAttackPwr 94
+   *@param baseDefensePwr 122
+   *@param baseStaminaPwr 88
+   */
    protected Squirtle(String species, String name, int num, 
        double ht, double wt, int baseAttackPwr, int baseDefensePwr, 
        int baseStaminaPwr) {
        
-      super(species, name, num, WATER_COLOR, ht, wt, WATER_TYPE,"", 
+      super(species, name, num, WATER_COLOR, ht, wt, WATER_TYPE, NULL_TYPE, 
          baseAttackPwr, baseDefensePwr, baseStaminaPwr);
-
+   
       chooseFastAttack();
       chooseSpecialAttack();
    }
-   
+   /**
+   *method to choose fast attack.
+   */
    protected void chooseFastAttack() {
       Random randGen = new Random();
       int index;
@@ -57,7 +80,9 @@ public class Squirtle extends Pokemon implements WaterType,NullType{
       fastAttackPower = WATER_FAST_ATK_POWER[index];
    
    }
-   //fast attack 
+   /**
+   *method to do fast attack
+   */
    public String performFastAttack(Pokemon victim) {
       
       Random rand = new Random();
@@ -73,16 +98,16 @@ public class Squirtle extends Pokemon implements WaterType,NullType{
       s = name + " performed " + fastAttack + " on " + victim.getSpecies();
       //check effectiveness of attack
       
-         if (vType.equals("Ground") || vType.equals("Rock") 
+      if (vType.equals("Ground") || vType.equals("Rock") 
              || vType.equals("Fire")) {
-            s = s + "\n It was super effective!";
-            modifier = modifier * 2.0; 
+         s = s + "\n It was super effective!";
+         modifier = modifier * 2.0; 
                      
-         } else if ( vType.equals("Water") || vType.equals("Dragon") ||
+      } else if ( vType.equals("Water") || vType.equals("Dragon") ||
              vType.equals("Grass")|| vType.equals("Dragon")){ 
-            s = s + "\n It was not very effective.";
-            modifier = modifier * 0.5;
-         }
+         s = s + "\n It was not very effective.";
+         modifier = modifier * 0.5;
+      }
        
       
       //bulbapedia damage formula:
@@ -92,7 +117,11 @@ public class Squirtle extends Pokemon implements WaterType,NullType{
       victim.beAttacked((int) damage);
       return s;
    }
-
+   
+   
+   /**
+   *method to choose special attack.
+   */
    protected void chooseSpecialAttack() {
     
       Random randGen = new Random();
@@ -102,10 +131,12 @@ public class Squirtle extends Pokemon implements WaterType,NullType{
       index = randGen.nextInt(WATER_SPECIAL_ATTACKS.length);
       specialAttack = WATER_SPECIAL_ATTACKS[index];
       specialAttackPower = WATER_SPECIAL_ATK_POWER[index];
-
+   
    }
 
-   
+   /**
+   *method to do special attack.
+   */
    public String performSpecialAttack(Pokemon victim) {
       Random rand = new Random();
       double damage = 0.0;
@@ -121,18 +152,19 @@ public class Squirtle extends Pokemon implements WaterType,NullType{
       s = name + " performed " + specialAttack + " on " + victim.getSpecies();
       
       //check effectiveness of attack
-               if (vType.equals("Fire") || vType.equals("Rock") 
+      if (vType.equals("Fire") || vType.equals("Rock") 
              || vType.equals("Ground")) {
              
-            s = s + "\n It was super effective!";
-            modifier = modifier * 2.0;          
-         } if (vType.equals("Water") || vType.equals("Dragon")|| vType.equals("Grass")
+         s = s + "\n It was super effective!";
+         modifier = modifier * 2.0;          
+      } 
+      if (vType.equals("Water") || vType.equals("Dragon")|| vType.equals("Grass")
          ||vType2.equals("Grass") || vType2.equals("Dragon") || vType2.equals("Water"))
          
-         {    
-            s = s + "\n It was not very effective.";
-            modifier = modifier * 0.5;
-         }
+      {    
+         s = s + "\n It was not very effective.";
+         modifier = modifier * 0.5;
+      }
       
      
       //check for same types for bonus
